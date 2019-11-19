2d2c882f9e6b3f1f5ae382422f92bc9168371acf
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2019-11-19 09:25:28 +0100

    Add doc.

--- R/trackReaders.R
+++ R/trackReaders.R
@@ -1,4 +1,11 @@
+#' Read tracts in .VTP format
+#'
+#' @param file A path string to the `.vtp` file.
+#'
+#' @return A \code{\link[tibble]{tibble}} storing the set of tracts.
 #' @export
+#'
+#' @examples
 read_vtp <- function(file) {
   file <- normalizePath(file)
   res <- ReadVTP(file)
