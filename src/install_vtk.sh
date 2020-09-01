7a4659337db444c8066ba550d1fc6fe5ad1cbffd
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-01 19:48:37 +0200

    Attempt to fix build.

--- src/install_vtk.sh
+++ src/install_vtk.sh
@@ -19,6 +19,26 @@ rm -fr vtk-build
 rm -fr ../inst/vtk
 ${CMAKE_BIN} \
 	-D BUILD_SHARED_LIBS=OFF \
+	-D VTK_ENABLE_WRAPPING=OFF \
+	-D VTK_GROUP_ENABLE_Imaging=NO \
+	-D VTK_GROUP_ENABLE_MPI=NO \
+	-D VTK_GROUP_ENABLE_Qt=NO \
+	-D VTK_GROUP_ENABLE_Rendering=NO \
+	-D VTK_GROUP_ENABLE_StandAlone=NO \
+	-D VTK_GROUP_ENABLE_Views=NO \
+	-D VTK_GROUP_ENABLE_Web=NO \
+	-D VTK_LEGACY_SILENT=ON \
+	-D VTK_MODULE_ENABLE_VTK_CommonCore=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonDataModel=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonExecutionModel=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonMath=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonMisc=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonSystem=YES \
+	-D VTK_MODULE_ENABLE_VTK_CommonTransforms=YES \
+	-D VTK_MODULE_ENABLE_VTK_IOCore=YES \
+	-D VTK_MODULE_ENABLE_VTK_IOLegacy=YES \
+	-D VTK_MODULE_ENABLE_VTK_IOXML=YES \
+	-D VTK_MODULE_ENABLE_VTK_IOXMLParser=YES \
 	-S vtk-src \
 	-B vtk-build
 ${CMAKE_BIN} --build vtk-build -j ${NCORES} --clean-first --config Release
