efe7143ad8d30c0b25ab9b16dc9e559e921f3ccb
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-02 18:18:16 +0200

    Try a hack for fixing build on windows.

--- src/generate_makevars.sh
+++ src/generate_makevars.sh
@@ -3,26 +3,25 @@
 # Generate the Makevars file
 
 VTK_VERSION=$1
-MAKEVARS_EXTENSION=$2
 
-echo "PKG_CPPFLAGS = \$(CPPFLAGS) -Ivtk/include/vtk-${VTK_VERSION}" > Makevars${MAKEVARS_EXTENSION}
-echo "PKG_LIBS = -Lvtk/lib -lvtk" >> Makevars${MAKEVARS_EXTENSION}
-echo "VTK_LIB = vtk/lib/libvtk.a" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo "PKG_CXXFLAGS = \$(CXX_VISIBILITY)" >> Makevars${MAKEVARS_EXTENSION}
-echo "PKG_CFLAGS = \$(C_VISIBILITY)" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
+echo "PKG_CPPFLAGS = \$(CPPFLAGS) -Ivtk/include/vtk-${VTK_VERSION}" > Makevars
+echo "PKG_LIBS = -Lvtk/lib -lvtk" >> Makevars
+echo "VTK_LIB = vtk/lib/libvtk.a" >> Makevars
+echo "" >> Makevars
+echo "PKG_CXXFLAGS = \$(CXX_VISIBILITY)" >> Makevars
+echo "PKG_CFLAGS = \$(C_VISIBILITY) \$(subst 64,-D__USE_MINGW_ANSI_STDIO,\$(subst 32,64,\$(WIN)))" >> Makevars
+echo "" >> Makevars
 TRIO_LIBS="BIN_LIBS = `find vtk-build -name "*.o"`"
-echo ${TRIO_LIBS} >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo ".PHONY: all" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo "all: \$(SHLIB)" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo "\$(SHLIB): \$(VTK_LIB)" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo "\$(VTK_LIB): \$(BIN_LIBS)" >> Makevars${MAKEVARS_EXTENSION}
-echo "	\$(AR) rcs \$(VTK_LIB) \$(BIN_LIBS)" >> Makevars${MAKEVARS_EXTENSION}
-echo "" >> Makevars${MAKEVARS_EXTENSION}
-echo "clean:" >> Makevars${MAKEVARS_EXTENSION}
-echo "	rm -f \$(BIN_LIBS) \$(SHLIB) \$(OBJECTS) \$(VTK_LIB)" >> Makevars${MAKEVARS_EXTENSION}
+echo ${TRIO_LIBS} >> Makevars
+echo "" >> Makevars
+echo ".PHONY: all" >> Makevars
+echo "" >> Makevars
+echo "all: \$(SHLIB)" >> Makevars
+echo "" >> Makevars
+echo "\$(SHLIB): \$(VTK_LIB)" >> Makevars
+echo "" >> Makevars
+echo "\$(VTK_LIB): \$(BIN_LIBS)" >> Makevars
+echo "	\$(AR) rcs \$(VTK_LIB) \$(BIN_LIBS)" >> Makevars
+echo "" >> Makevars
+echo "clean:" >> Makevars
+echo "	rm -f \$(BIN_LIBS) \$(SHLIB) \$(OBJECTS) \$(VTK_LIB)" >> Makevars
