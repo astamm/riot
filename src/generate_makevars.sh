4315b3420d179c0a58a6c7bf142532734114d7c9
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-03 16:01:06 +0200

    Puzzled by makevars fail on win.

--- src/generate_makevars.sh
+++ src/generate_makevars.sh
@@ -3,10 +3,12 @@
 # Generate the Makevars file
 
 echo "PKG_CPPFLAGS = \$(CPPFLAGS) -Ivtk/include" > Makevars
-echo "PKG_LIBS = vtk/lib/libvtk.a" >> Makevars
+#echo "PKG_LIBS = vtk/lib/libvtk.a" >> Makevars
+echo "PKG_LIBS = -Lvtk/lib -lvtk" >> Makevars
+echo "VTK_LIB = vtk/lib/libvtk.a" >> Makevars
 echo "" >> Makevars
 echo "PKG_CXXFLAGS = \$(CXX_VISIBILITY)" >> Makevars
-echo "PKG_CFLAGS = \$(C_VISIBILITY) \$(subst 64,-D__USE_MINGW_ANSI_STDIO,\$(subst 32,64,\$(WIN)))" >> Makevars
+echo "PKG_CFLAGS = \$(C_VISIBILITY)" >> Makevars
 echo "" >> Makevars
 echo "VTK_LIBS = `find vtk/lib -type f -name "*.o" -o -name "*.obj" | xargs`" >> Makevars
 echo "" >> Makevars
@@ -14,10 +16,10 @@ echo ".PHONY: all" >> Makevars
 echo "" >> Makevars
 echo "all: \$(SHLIB)" >> Makevars
 echo "" >> Makevars
-echo "\$(SHLIB): \$(PKG_LIBS)" >> Makevars
+echo "\$(SHLIB): \$(VTK_LIB)" >> Makevars
 echo "" >> Makevars
-echo "\$(PKG_LIBS): \$(VTK_LIBS)" >> Makevars
-echo "	\$(AR) rcs \$(PKG_LIBS) \$(VTK_LIBS)" >> Makevars
+echo "\$(VTK_LIB): \$(VTK_LIBS)" >> Makevars
+echo "	\$(AR) rcs \$(VTK_LIB) \$(VTK_LIBS)" >> Makevars
 echo "" >> Makevars
 echo "clean:" >> Makevars
-echo "	rm -f \$(VTK_LIBS) \$(SHLIB) \$(OBJECTS) \$(PKG_LIBS)" >> Makevars
+echo "	rm -f \$(VTK_LIBS) \$(SHLIB) \$(OBJECTS) \$(VTK_LIB)" >> Makevars
