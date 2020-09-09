35c9de2b6afdcaa1195b6e88f7ece20375a66c09
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-09 11:36:29 +0200

    Keep cmake approach.

--- src/install_vtk.sh
+++ src/install_vtk.sh
@@ -47,7 +47,9 @@ ${CMAKE_BIN} \
 ${CMAKE_BIN} --build vtk-build -j ${NCORES} --config Release
 ${CMAKE_BIN} --install vtk-build --prefix vtk
 
-rm -rf vtk/lib/*
+rm -fr vtk/lib/*
+cp -r vtk/include/vtk/* vtk/include
+rm -fr vtk/include/vtk
 cp -r `find vtk-build -name "*.o" -o -name "*.obj" | xargs` vtk/lib
 
 rm -fr vtk-src vtk-build
