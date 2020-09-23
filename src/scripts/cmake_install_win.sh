5b1c5a31002abffdaace6029b38f6075b4632bf4
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-23 13:29:03 +0200

    Use cmake binaries for windows.

--- src/scripts/cmake_install_win.sh
+++ src/scripts/cmake_install_win.sh
@@ -3,14 +3,16 @@
 RSCRIPT_BIN=$1
 NCORES=`${RSCRIPT_BIN} -e "cat(parallel::detectCores(logical = FALSE))"`
 
-${RSCRIPT_BIN} -e "utils::download.file(
-    url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2.tar.gz',
-    destfile = 'cmake.tar.gz')"
-
-${RSCRIPT_BIN} -e "utils::untar('cmake.tar.gz')"
-mv cmake-3.18.2 cmake
-
-cd cmake
-./bootstrap -- -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_USE_OPENSSL=OFF -DCMAKE_C_STANDARD=11
-make -j${NCORES}
-cd ..
+if [[ ${R_ARCH} =~ "x64" ]]; then
+    ${RSCRIPT_BIN} -e "utils::download.file(
+        url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win64-x64.zip',
+        destfile = 'cmake.zip')"
+    ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
+    mv cmake-3.18.2-win64-x64 cmake
+else
+    ${RSCRIPT_BIN} -e "utils::download.file(
+        url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win32-x86.zip',
+        destfile = 'cmake.zip')"
+    ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
+    mv cmake-3.18.2-win32-x86 cmake
+fi
