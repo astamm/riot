2e54dc7e8291e630028744686d07eb1f2f5f515d
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-24 15:01:43 +0200

    Provide compiler paths on windows for cmake build.

--- src/scripts/cmake_install_win.sh
+++ src/scripts/cmake_install_win.sh
@@ -1,19 +1,28 @@
 #! /bin/sh
 
 RSCRIPT_BIN=$1
-R_BIN=$2
-NCORES=`${RSCRIPT_BIN} -e "cat(parallel::detectCores(logical = FALSE))"`
+CC=$2
+CXX=$3
+MAKE=$4
+# R_BIN=$2
+# NCORES=`${RSCRIPT_BIN} -e "cat(parallel::detectCores(logical = FALSE))"`
 
-if [[ ${R_ARCH} =~ "x64" ]]; then
-    ${RSCRIPT_BIN} -e "utils::download.file(
-        url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win64-x64.zip',
-        destfile = 'cmake.zip')"
-    ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
-    mv cmake-3.18.2-win64-x64 cmake
-else
-    ${RSCRIPT_BIN} -e "utils::download.file(
-        url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win32-x86.zip',
-        destfile = 'cmake.zip')"
-    ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
-    mv cmake-3.18.2-win32-x86 cmake
-fi
+
+cd cmake
+./bootstrap CC="${CC}" CXX="${CXX}" MAKE="${MAKE}" -- -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_USE_OPENSSL=OFF
+make -j${NCORES}
+cd ..
+
+# if [[ ${R_ARCH} =~ "x64" ]]; then
+#     ${RSCRIPT_BIN} -e "utils::download.file(
+#         url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win64-x64.zip',
+#         destfile = 'cmake.zip')"
+#     ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
+#     mv cmake-3.18.2-win64-x64 cmake
+# else
+#     ${RSCRIPT_BIN} -e "utils::download.file(
+#         url = 'https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-win32-x86.zip',
+#         destfile = 'cmake.zip')"
+#     ${RSCRIPT_BIN} -e "utils::unzip('cmake.zip')"
+#     mv cmake-3.18.2-win32-x86 cmake
+# fi
