83a3064112131616b20aa81be97152428cec7ab1
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-09-24 14:17:41 +0200

    Added forgotten WIN flags.

--- src/scripts/vtk_install.sh
+++ src/scripts/vtk_install.sh
@@ -2,18 +2,19 @@
 
 CMAKE_BIN=$1
 NCORES=$2
+WIN=$3
 
 ${CMAKE_BIN} --build vtk-build -j ${NCORES} --config Release
-${CMAKE_BIN} --install vtk-build --prefix vtk
+${CMAKE_BIN} --install vtk-build --prefix vtk${WIN}
 
-LIB_FOLDER=`ls -d vtk/lib*`
-if [[ $LIB_FOLDER != "vtk/lib" ]]; then
-    mv $LIB_FOLDER vtk/lib
+LIB_FOLDER=`ls -d vtk${WIN}/lib*`
+if [[ $LIB_FOLDER != "vtk${WIN}/lib" ]]; then
+    mv $LIB_FOLDER vtk${WIN}/lib
 fi
-rm -fr vtk/lib/*
-cp -r vtk/include/vtk/* vtk/include
-rm -fr vtk/include/vtk
-cp -r `find vtk-build -name "*.o" -o -name "*.obj" | xargs` vtk/lib
+rm -fr vtk${WIN}/lib/*
+cp -r vtk${WIN}/include/vtk/* vtk${WIN}/include
+rm -fr vtk${WIN}/include/vtk
+cp -r `find vtk-build -name "*.o" -o -name "*.obj" | xargs` vtk${WIN}/lib
 
 rm -fr vtk-src vtk-build
 rm -f vtk-src.tar.gz
