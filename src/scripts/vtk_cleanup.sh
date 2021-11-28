211aebb63fe6043fb7568d61841fb8722b3236fb
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-11-28 21:15:42 +0100

    Still trying to trick header includes.

--- src/scripts/vtk_cleanup.sh
+++ src/scripts/vtk_cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p ../inst/include
-cp -r vtk${R_ARCH}/include/vtk/* ../inst/include/
+mkdir -p ../inst/include/vtk
+cp -r vtk${R_ARCH}/include/vtk/* ../inst/include/vtk/
 rm -fr vtk-src
 rm -f vtk-src.tar.gz
