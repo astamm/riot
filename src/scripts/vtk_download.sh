c2b2c1f10c5916fbdce1b8d5021be67054cd4a49
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-02-01 22:03:15 +0100

    Try setting up properly VTK9 without OpenGL.

--- src/scripts/vtk_download.sh
+++ src/scripts/vtk_download.sh
@@ -2,14 +2,14 @@
 
 RSCRIPT_BIN=$1
 
-# # Download VTK source
-# ${RSCRIPT_BIN} -e "utils::download.file(
-#     url = 'https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz',
-#     destfile = 'vtk-src.tar.gz')"
-#
-# # Uncompress VTK source
-# ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
-# mv VTK-9.1.0 vtk-src
+# Download VTK source
+${RSCRIPT_BIN} -e "utils::download.file(
+    url = 'https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz',
+    destfile = 'vtk-src.tar.gz')"
+
+# Uncompress VTK source
+${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
+mv VTK-9.1.0 vtk-src
 
 # # Download VTK source
 # ${RSCRIPT_BIN} -e "utils::download.file(
@@ -20,11 +20,11 @@ RSCRIPT_BIN=$1
 # ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
 # mv VTK-7.1.1 vtk-src
 
-# Download VTK source
-${RSCRIPT_BIN} -e "utils::download.file(
-    url = 'https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz',
-    destfile = 'vtk-src.tar.gz')"
-
-# Uncompress VTK source
-${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
-mv VTK-8.2.0 vtk-src
+# # Download VTK source
+# ${RSCRIPT_BIN} -e "utils::download.file(
+#     url = 'https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz',
+#     destfile = 'vtk-src.tar.gz')"
+#
+# # Uncompress VTK source
+# ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
+# mv VTK-8.2.0 vtk-src
