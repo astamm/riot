4691092372d5142daca5b3cf0177e1f975353581
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-02-01 17:45:14 +0100

    Try VTK 8.

--- src/scripts/vtk_download.sh
+++ src/scripts/vtk_download.sh
@@ -11,11 +11,20 @@ RSCRIPT_BIN=$1
 # ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
 # mv VTK-9.1.0 vtk-src
 
+# # Download VTK source
+# ${RSCRIPT_BIN} -e "utils::download.file(
+#     url = 'https://www.vtk.org/files/release/7.1/VTK-7.1.1.tar.gz',
+#     destfile = 'vtk-src.tar.gz')"
+#
+# # Uncompress VTK source
+# ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
+# mv VTK-7.1.1 vtk-src
+
 # Download VTK source
 ${RSCRIPT_BIN} -e "utils::download.file(
-    url = 'https://www.vtk.org/files/release/7.1/VTK-7.1.1.tar.gz',
+    url = 'https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz',
     destfile = 'vtk-src.tar.gz')"
 
 # Uncompress VTK source
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
-mv VTK-7.1.1 vtk-src
+mv VTK-8.2.0 vtk-src
