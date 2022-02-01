76135b15392143ebcd588064d6a21265ba6d1b59
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-02-01 17:21:47 +0100

    Try using VTK 7.

--- src/scripts/vtk_download.sh
+++ src/scripts/vtk_download.sh
@@ -2,11 +2,20 @@
 
 RSCRIPT_BIN=$1
 
+# # Download VTK source
+# ${RSCRIPT_BIN} -e "utils::download.file(
+#     url = 'https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz',
+#     destfile = 'vtk-src.tar.gz')"
+#
+# # Uncompress VTK source
+# ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
+# mv VTK-9.1.0 vtk-src
+
 # Download VTK source
 ${RSCRIPT_BIN} -e "utils::download.file(
-    url = 'https://www.vtk.org/files/release/9.1/VTK-9.1.0.tar.gz',
+    url = 'https://www.vtk.org/files/release/7.1/VTK-7.1.1.tar.gz',
     destfile = 'vtk-src.tar.gz')"
 
 # Uncompress VTK source
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
-mv VTK-9.1.0 vtk-src
+mv VTK-7.1.1 vtk-src
