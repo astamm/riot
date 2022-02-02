7bbbfbe58a47b041dc746794be3aad41bc55aec2
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-02-02 14:07:24 +0100

    Clean makevars

--- src/scripts/vtk_install.sh
+++ src/scripts/vtk_install.sh
@@ -7,13 +7,15 @@ ARCH=$3
 "${CMAKE_BIN}" --build vtk-build${ARCH} -j ${NCORES} --config Release
 "${CMAKE_BIN}" --install vtk-build${ARCH} --prefix vtk${ARCH}
 
+echo "Clean install folder"
 LIB_FOLDER=`ls -d vtk${ARCH}/lib*`
-if [ $LIB_FOLDER != "vtk/lib" ]; then
+if [ $LIB_FOLDER != "vtk${ARCH}/lib" ]; then
     mv $LIB_FOLDER vtk${ARCH}/lib
 fi
 rm -fr vtk${ARCH}/lib/*
 cp -r vtk${ARCH}/include/vtk/* vtk${ARCH}/include
 rm -fr vtk${ARCH}/include/vtk
-cp -r `find vtk${ARCH}-build -name "*.o" -o -name "*.obj" | xargs` vtk${ARCH}/lib
 
-rm -fr vtk${ARCH}-build
+echo "Move object files in vtk/lib folder"
+cp -r `find vtk-build${ARCH} -name "*.o" -o -name "*.obj" | xargs` vtk${ARCH}/lib
+rm -fr vtk-build${ARCH}
