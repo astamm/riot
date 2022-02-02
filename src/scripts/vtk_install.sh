9739e63d077f35a232ec49462ef5409b55b91b16
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-02-02 10:51:30 +0100

    Compile all .o files into single static lib to avoid ordering issues.

--- src/scripts/vtk_install.sh
+++ src/scripts/vtk_install.sh
@@ -4,6 +4,14 @@ CMAKE_BIN=$1
 NCORES=$2
 ARCH=$3
 
-"${CMAKE_BIN}" --build vtk${ARCH}-build -j ${NCORES} --config Release
-"${CMAKE_BIN}" --install vtk${ARCH}-build --prefix vtk${ARCH}
+"${CMAKE_BIN}" --build vtk-build${ARCH} -j ${NCORES} --config Release
+"${CMAKE_BIN}" --install vtk-build${ARCH} --prefix vtk${ARCH}
+
+LIB_FOLDER=`ls -d vtk${ARCH}/lib*`
+mv $LIB_FOLDER vtk${ARCH}/lib
+rm -fr vtk${ARCH}/lib/*
+cp -r vtk${ARCH}/include/vtk/* vtk${ARCH}/include
+rm -fr vtk${ARCH}/include/vtk
+cp -r `find vtk${ARCH}-build -name "*.o" -o -name "*.obj" | xargs` vtk${ARCH}/lib
+
 rm -fr vtk${ARCH}-build
