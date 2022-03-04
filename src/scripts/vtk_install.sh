50c875cfe4eecd854aacd27b9c128246a65b2904
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-03-04 17:35:30 +0100

    Clean some files.

--- src/scripts/vtk_install.sh
+++ src/scripts/vtk_install.sh
@@ -6,16 +6,9 @@ NCORES=$2
 "${CMAKE_BIN}" --build vtk-build -j ${NCORES} --config Release
 "${CMAKE_BIN}" --install vtk-build --prefix vtk
 
-echo "Clean install folder"
 LIB_FOLDER=`ls -d vtk/lib*`
 if [ $LIB_FOLDER != "vtk/lib" ]; then
     mv $LIB_FOLDER vtk/lib
 fi
-#rm -fr vtk/lib/*
-#cp -r vtk/include/vtk/* vtk/include
-#rm -fr vtk/include/vtk
-
-#echo "Move object files in vtk/lib folder"
-#cp -r `find vtk-build -name "*.o" -o -name "*.obj" | xargs` vtk/lib
 
 rm -fr vtk-build
