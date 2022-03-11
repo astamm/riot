e63e16319904c2d97f915e3feba1d2a3d72529b1
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-03-11 07:20:46 +0100

    Avoid checking for array bounds in problematic VTK source files.

--- src/scripts/vtk_download.sh
+++ src/scripts/vtk_download.sh
@@ -20,3 +20,7 @@ echo 'typedef int make_iso_compilers_happy;' | cat - vtk-src/ThirdParty/expat/vt
 echo '#include <limits>' | cat - vtk-src/Common/Core/vtkGenericDataArrayLookupHelper.h > temp && mv temp vtk-src/Common/Core/vtkGenericDataArrayLookupHelper.h
 echo '#include <limits>' | cat - vtk-src/Common/Core/vtkDataArrayPrivate.txx > temp && mv temp vtk-src/Common/Core/vtkDataArrayPrivate.txx
 echo '#include <limits>' | cat - vtk-src/Common/DataModel/vtkPiecewiseFunction.cxx > temp && mv temp vtk-src/Common/DataModel/vtkPiecewiseFunction.cxx
+
+# Do not check for array bounds for two files in DataModel module
+echo 'set_soure_file_properties(vtkHyperTreeGrid.cxx PROPERTIES COMPILE_FLAGS -Wno-array-bounds)' | cat - vtk-src/Common/DataModel/CMakeLists.txt > temp && mv temp vtk-src/Common/DataModel/CMakeLists.txt
+echo 'set_soure_file_properties(vtkPolyhedron.cxx PROPERTIES COMPILE_FLAGS -Wno-array-bounds)' | cat - vtk-src/Common/DataModel/CMakeLists.txt > temp && mv temp vtk-src/Common/DataModel/CMakeLists.txt
