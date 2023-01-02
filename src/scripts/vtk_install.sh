df6fd791dd9b35199df1b15342d5436e3816823c
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2023-01-02 21:57:05 +0100

    Remove useless calls to set ncores.

--- src/scripts/vtk_install.sh
+++ src/scripts/vtk_install.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
 CMAKE_BIN=$1
-NCORES=$2
 
-"${CMAKE_BIN}" --build vtk-build -j ${NCORES} --config Release
+"${CMAKE_BIN}" --build vtk-build --config Release
 "${CMAKE_BIN}" --install vtk-build --prefix vtk
 
 LIB_FOLDER=`ls -d vtk/lib*`
