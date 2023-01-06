b5b69d097ae4f145c5a6008081bae4d7dff8c4ce
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2023-01-06 10:57:40 +0100

    Correctly disable warnings on LLVM clang compiler.

--- src/scripts/vtk_download.sh
+++ src/scripts/vtk_download.sh
@@ -2,21 +2,23 @@
 
 RSCRIPT_BIN=$1
 VERSION=9.2
-THIRD=4
+PATCH=4
 
 # Download VTK source
 ${RSCRIPT_BIN} -e "utils::download.file(
-    url = 'https://www.vtk.org/files/release/${VERSION}/VTK-${VERSION}.${THIRD}.tar.gz',
+    url = 'https://www.vtk.org/files/release/${VERSION}/VTK-${VERSION}.${PATCH}.tar.gz',
     destfile = 'vtk-src.tar.gz')"
 
 # Uncompress VTK source
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'vtk-src.tar.gz')"
-mv VTK-${VERSION}.${THIRD} vtk-src
+mv VTK-${VERSION}.${PATCH} vtk-src
 rm -f vtk-src.tar.gz
 
 # Do not check for deprecated-non-prototype and strict-prototypes in vtkzlib
+# when using LLVM Clang compiler
+echo '
 if (CMAKE_C_COMPILER_ID STREQUAL "Clang")
-  echo '
   set_source_files_properties(adler32.c compress.c crc32.c deflate.c gzclose.c gzlib.c gzread.c gzwrite.c inflate.c infback.c inftrees.c inffast.c trees.c uncompr.c zutil.c PROPERTIES COMPILE_FLAGS "-Wno-deprecated-non-prototype -Wno-strict-prototypes")
-' | cat - vtk-src/ThirdParty/zlib/vtkzlib/CMakeLists.txt > temp && mv temp vtk-src/ThirdParty/zlib/vtkzlib/CMakeLists.txt
 endif()
+' | cat - vtk-src/ThirdParty/zlib/vtkzlib/CMakeLists.txt > temp && mv temp vtk-src/ThirdParty/zlib/vtkzlib/CMakeLists.txt
+
